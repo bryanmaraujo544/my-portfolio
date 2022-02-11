d489ed5837f435a60355223a09dd2e5fc203ee2e
Bryan Martins <bryanmaraujo544@gmail.com>
2022-02-11 16:15:19 -0300

    paving the way to start project

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const global = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: 'Courier Prime', monospace, sans-serif;
+  }
+`;
+
+export default global;
