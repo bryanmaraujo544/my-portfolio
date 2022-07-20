fd0edacfeae6d89ee792fd94aa3cd7ca93c59912
Bryan Martins <bryanmaraujo544@gmail.com>
2022-07-19 21:54:44 -0300

    adding google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from 'styles/global';
+import Script from 'next/script';
 import { SettingsContextProvider } from '../contexts/SettingsContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GA});
+          `}
+      </Script>
       <GlobalStyles />
       <SettingsContextProvider>
         <Component {...pageProps} />
