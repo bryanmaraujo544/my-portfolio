69bd3bb046470a3a893ea71623e2f68ba2e14010
Bryan Martins <110990221+bryanmaraujo544@users.noreply.github.com>
2022-09-28 10:41:48 -0300

    Update text-content.tsx

--- src/sections/Contact/text-content.tsx
+++ src/sections/Contact/text-content.tsx
@@ -13,7 +13,7 @@ const textContent: Props = {
     title: 'Contate-me',
     description: (
       <>
-        Estou aberto à oportunidades de emprego. Caso queira me contatar esteja
+        Não estou aberto à oportunidades de emprego. mas caso queira entrar em contato esteja
         à vontade para me mandar um email em <b>bryanmaraujo544@gmail.com</b>,
         ou nas minhas redes sociais abaixo.
       </>
@@ -23,7 +23,7 @@ const textContent: Props = {
     title: 'Contact me',
     description: (
       <>
-        I am currently looking for a job and I am available for any conversation
+        Currently I'm not looking for a job, but I am available for any conversation
         about. You can reach me for my email: <b>bryanmaraujo544@gmail.com</b>,
         or in one of my social medias below.
       </>
